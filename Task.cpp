c1cf49518123e1045e36622ee9604560fef99dcb
Brian Hao <brianhao@gmail.com>
2016-10-21 16:50:38 -0400

    Implemented GenericTask and its subclasses

--- Task.cpp
+++ Task.cpp
@@ -1,12 +1,116 @@
+/****************/
+/** TASK CLASS **/
+/****************/
+/*
+ =SUPERCLASS=
+ This class implements a Task item. The default task is a Generic Task, which contains
+ a description, as well as a deadline in the form of an integer representing days-until-due.
+ It has functions to provide data about itself.
+ 
+ =SUBCLASSES=
+ Inheriting off a Generic Task are Shopping, Event and Homework Tasks. A Shopping Task
+ contains a shopping list of items to be bought. An Event Task has a time and location.
+ Finally, a Homework Task has a class subject associated with it. In additional to their
+ class-exclusive parameters, they contain overridden functions that allow them to provide
+ data about themselves
+ */
+
 #ifndef TASK_CPP
 #define TASK_CPP
 
 #include "Task.h"
+#include <iostream>
 #include <string>
 #include <vector>
-#include "UniqueVector.h"
 using namespace std;
 
-//Implementation
+/******************/
+/** Generic Task **/
+/******************/
+// Simple output for Generic Tasks.
+void Task::print() {
+    string day = (deadline > 1) ? " days " : " day ";
+    cout << "(" << deadline << day << "from now) " << description << endl;
+}
+
+// Save format for Generic Tasks.
+string Task::saveFormat() {
+    string save = "G|" + to_string(deadline) + "|" + description;
+    return save;
+}
+
+/*******************/
+/** Shopping Task **/
+/*******************/
+// Simple output for Shopping Tasks.
+void ShoppingTask::print() {
+    string day = (deadline > 1) ? " days " : " day ";
+    cout << "(" << deadline << day << "from now) [Shopping] " << description << endl;
+}
+
+void ShoppingTask::printDetailed() {
+    print();
+    cout << "ITEMS TO PURCHASE:" << endl;
+    // Iterates over shopping list
+    if (shoppingList.size() > 0) {
+        for (int i = 0; i < shoppingList.size(); i++) {
+            cout << shoppingList[i] << endl;
+        }
+    }
+}
+
+// Save format for Shopping Tasks.
+string ShoppingTask::saveFormat() {
+    string save = "S|" + to_string(deadline) + "|" + description;
+    // Iterates over shopping list
+    if (shoppingList.size() > 0) {
+        for (int i = 0; i < shoppingList.size(); i++) {
+            save += "|" + shoppingList[i];
+        }
+    }
+    return save;
+}
+
+/****************/
+/** Event Task **/
+/****************/
+// Simple output for Event Tasks.
+void EventTask::print() {
+    string day = (deadline > 1) ? " days " : " day ";
+    cout << "(" << deadline << day << "from now) [Event] " << description << endl;
+}
+
+void EventTask::printDetailed() {
+    print();
+    cout << "WHERE: " << location << endl << "WHEN: " << time << endl;
+}
+
+// Save format for Event Tasks.
+string EventTask::saveFormat() {
+    string save = "G|" + to_string(deadline) + "|" + description + "|" + location + "|" + time;
+    return save;
+}
+
+
+/*******************/
+/** Homework Task **/
+/*******************/
+// Simple output for Homework Tasks.
+void HomeworkTask::print() {
+    string day = (deadline > 1) ? " days " : " day ";
+    cout << "(" << deadline << day << "from now) [Homework] " << description << endl;
+}
+
+void HomeworkTask::printDetailed() {
+    print();
+    cout << "SUBJECT: " << subject << endl;
+}
+
+// Save format for Homework Tasks.
+string HomeworkTask::saveFormat() {
+    string save = "G|" + to_string(deadline) + "|" + description + "|" + subject;
+    return save;
+}
+
 
 #endif
--- Task.h
+++ Task.h
@@ -2,7 +2,17 @@
 /** TASK CLASS **/
 /****************/
 /*
- This class
+ =SUPERCLASS=
+ This class implements a Task item. The default task is a Generic Task, which contains
+ a description, as well as a deadline in the form of an integer representing days-until-due.
+ It has functions to provide data about itself.
+ 
+ =SUBCLASSES=
+ Inheriting off a Generic Task are Shopping, Event and Homework Tasks. A Shopping Task
+ contains a shopping list of items to be bought. An Event Task has a time and location.
+ Finally, a Homework Task has a class subject associated with it. In additional to their
+ class-exclusive parameters, they contain overridden functions that allow them to provide
+ data about themselves
  */
 
 #ifndef TASK_H
@@ -10,67 +20,105 @@
 
 #include <string>
 #include <vector>
-#include "UniqueVector.h"
 using namespace std;
 
     /******************/
     /** Generic Task **/
     /******************/
-
-//A Generic Task has a description and a deadline
 class Task {
 public:
+    // Constructor and Destructor
     Task();
     virtual ~Task();
-    void setDeadline(int inDeadline);
-    void setDescription(string inDescription);
-    int getDeadline() { return deadline; };
+    
+    // Setters and Getters
+    void setDeadline(int inDeadline) { deadline = inDeadline; };
+    void setDescription(string inDescription) { description = inDescription; };
+    unsigned int getDeadline() { return deadline; };
     string getDescription() { return description; };
+    
+    // Virtual functions to access task information
     virtual void print();
-    virtual void printDetailed();
+    virtual void printDetailed() { print(); };
+    virtual string saveFormat();
+    
     
 protected:
-    int deadline;
+    // Task variables, used for all types of Tasks
+    unsigned int deadline;
     string description;
 };
 
-//Shopping Task : Extends Generic Task
-//  A Shopping Task has a shopping list, handled by a UniqueVector.
-class Shopping: public Task {
+    /*******************/
+    /** Shopping Task **/
+    /*******************/
+class ShoppingTask: public Task {
 public:
-    Shopping();
-    virtual ~Shopping();
-    void addItem(string newItem);
+    // Constructor and Destructor
+    ShoppingTask();
+    virtual ~ShoppingTask();
+    
+    // Functions relating to the shopping list
+    void addItem(string newItem) { shoppingList.push_back(newItem); };
     unsigned long shoppingListLength() { return shoppingList.size(); };
+    
+    // Virtual functions to access task information
     virtual void print();
     virtual void printDetailed();
+    virtual string saveFormat();
     
 protected:
-    UniqueVector<string> shoppingList;
+    // ShoppingTask-specific variable
+    vector<string> shoppingList;
 };
 
-//Event Task : Extends Generic Task
-//  An Event Task has a location and a time
-class Event: public Task {
+    /****************/
+    /** Event Task **/
+    /****************/
+class EventTask: public Task {
 public:
-    Event();
-    virtual ~Event();
+    // Constructor and Destructor
+    EventTask();
+    virtual ~EventTask();
     
+    // Setters and Getters
+    void setLocation(string inLocation) { location = inLocation; };
+    void setTime(string inTime) { time = inTime; };
+    string getLocation() { return location; };
+    string getTime() { return time; };
+    
+    // Virtual functions to access task information
+    virtual void print();
+    virtual void printDetailed();
+    virtual string saveFormat();
     
 protected:
+    // EventTask-specific variables
     string location;
     string time;
 };
 
-//Homework Task : Extends Generic Task
-//  A Homework task has a class subject
-class Homework: public Task {
+    /*******************/
+    /** Homework Task **/
+    /*******************/
+class HomeworkTask: public Task {
 public:
-    Homework();
-    virtual ~Homework();
+    // Constructor and Destructor
+    HomeworkTask();
+    virtual ~HomeworkTask();
     
+    // Setters and Getters
+    void setSubject(string inSubject) { subject = inSubject; };
+    string getSubject() { return subject; };
+    
+    // Virtual functions to access task information
+    virtual void print();
+    virtual void printDetailed();
+    virtual string saveFormat();
+
 protected:
-    
+    //HomeworkTask-specific variable
+    string subject;
 };
 
 #endif
