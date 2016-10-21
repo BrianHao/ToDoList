88a067f6b286c78ea2b220f31e860a9d120a9b71
Brian Hao <brianhao@gmail.com>
2016-10-21 15:45:04 -0400

    Added comments to describe main.cpp functionality

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
+// The main.cpp's only function will be to create an instance of a ToDoListApp,
+// and then call ToDoListApp.run()
+
 #include <iostream>
 using namespace std;
 
 int main() {
+    // Create new instance of ToDoListApp
+    // Call ToDoListApp.run();
     return 0;
 }
